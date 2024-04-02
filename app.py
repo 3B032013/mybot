52832bfe25a0731587e34fe5acfe6fccbb4c892c
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2024-04-02 15:04:31 +0800

    初始提交

---
+++ app.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 15:04:03 2024
+
+@author: user
+"""
+
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebHookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+import bot_info
+
+app = Flask(__name__)
+
+# LINE Bot Basic Profile
+line_bot_api = LineBotApi(bot_info.line_bot_token)
+handler = WebHookHandler(bot_info.line_bot_secret)
+
+# Post Request from LINE Platform
+@app.route('/callback', methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info('Request body:' + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def echo(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+if __name__ == '__main__':
+    app.run()
---
+++ bot_info.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 15:04:03 2024
+
+@author: user
+"""
+
+# Channel Access Token
+line_bot_token = 'wyv4jV8H9YJJgyHIQmpmtShhEGLu8RcaQR1+c2InUF5Hfee8dkwt7D+hyWIds67oD9WYZv+xnOoC7yX0HJj3BzWb6INihwhrVgeDJ9nuLlZdix2qlEfUnULURGDHCw9zBs5/3nrQKlbYcxGWx6fJkgdB04t89/1O/w1cDnyilFU='
+# Channel Secret
+line_bot_secret = '4043c540d7a34659ff17cd5a6d3d6455'
